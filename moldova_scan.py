339d2bcdef87cbcf2b03c5ee16af330c4e29d301
rt3lpiz <rt3lpiz@mail.ru>
2017-05-29 19:02:05 +0300

    prima incercare

--- moldova_scan.py
+++ moldova_scan.py
@@ -7,11 +7,15 @@ for x in ip_list:
     if 'host-static' in socket.gethostbyaddr(str(x))[0]:
         pass
     else:
-        print(socket.gethostbyaddr(str(x)))
-        #print(type(socket.gethostbyaddr(str(x))[0]))
-        #print(type(socket.gethostbyaddr(str(x))[2][0]))
-        with open('rezultat.txt', 'a') as f:
-            f.write("IP: %s nume server: %s" %(socket.gethostbyaddr(str(x))[2][0],
+        try:
+            print(socket.gethostbyaddr(str(x)))
+            #print(type(socket.gethostbyaddr(str(x))[0]))
+            #print(type(socket.gethostbyaddr(str(x))[2][0]))
+            with open('rezultat.txt', 'a') as f:
+                f.write("IP: %s nume server: %s" %(socket.gethostbyaddr(str(x))[2][0],
                 socket.gethostbyaddr(str(x))[0]))
-            f.write('\n')
-            f.close()
+                f.write('\n')
+                f.close()
+        except Exception as e:
+            print(e)
+            pass
