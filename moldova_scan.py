694ed6bb86ca8e7ba572863fef9c1edbc0d7a88b
rt3lpiz <rt3lpiz@mail.ru>
2017-05-29 17:08:41 +0300

    prima incerare

---
+++ moldova_scan.py
@@ -0,0 +1,10 @@
+import netaddr
+import socket
+
+ip_list = netaddr.IPNetwork('5.56.64.0/18')
+
+for x in ip_list:
+    if 'host-static' in socket.gethostbyaddr(str(x))[0]:
+        pass
+    else:
+        print socket.gethostbyaddr(str(x))
